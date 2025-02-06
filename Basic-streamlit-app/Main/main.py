c43225eaed8d8aa6405af2cd36718bdc44b7dbd3
wsteadman <steady.william@gmail.com>
2025-02-06 12:37:41 -0500

    working on app

--- Basic-streamlit-app/Main/main.py
+++ Basic-streamlit-app/Main/main.py
@@ -1,1 +1,10 @@
+import streamlit as st
+import pandas as pd
 
+st.title("This is an app to analyze some penguins!")
+
+## Importing the Penguins Df using relative path
+df = pd.read_csv('Data\sample_data.csv')
+
+st.dataframe(df)
+## diplaying penguins df
