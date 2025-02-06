ec1cb41f8fedd455616cbd790bb281b749d6cb11
wsteadman <steady.william@gmail.com>
2025-02-06 17:23:49 -0500

    Made some sliders and filters

--- Basic-streamlit-app/main.py
+++ Basic-streamlit-app/main.py
@@ -6,9 +6,16 @@ st.title("This is an app to analyze some penguins!")
 ## Importing the Penguins Df using relative path
 df = pd.read_csv('Data\penguins.csv')
 
-st.dataframe(df)
-## diplaying penguins df
-
+## Creating a slider to display all penguins with less than or equal to flipper length
 flip_length = st.slider('Select a flipper range', min_value = df['flipper_length_mm'].min(), max_value = df['flipper_length_mm'].max())
-
 st.dataframe(df[df['flipper_length_mm'] <= flip_length])
+
+
+## Creating a button that will return the penguins with the exact flipper length of the slider
+if st.button('Click me for exact flipper length'):
+    df_2 = df[df['flipper_length_mm'] == flip_length]
+
+    if not df_2.empty:
+        st.dataframe(df_2)
+    else:
+        st.write('No penguin has that exact flipper length')
