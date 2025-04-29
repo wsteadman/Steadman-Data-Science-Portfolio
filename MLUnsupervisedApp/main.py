2598b2b4357129daa1e284707ac72eafe7c46e23
wsteadman <steady.william@gmail.com>
2025-04-29 09:58:48 -0400

    changed font size

--- MLUnsupervisedApp/main.py
+++ MLUnsupervisedApp/main.py
@@ -277,4 +277,4 @@ if st.sidebar.button("Run Clustering"):
             Keep in mind that KMeans doesn’t know these labels — it just groups similar data points based on feature values.
             """)
 else:
-    st.markdown("## 👈 Adjust parameters in the sidebar and click 'Run Clustering' to start analysis!")
+    st.markdown("### 👈 Adjust parameters in the sidebar and click 'Run Clustering' to start analysis!")
