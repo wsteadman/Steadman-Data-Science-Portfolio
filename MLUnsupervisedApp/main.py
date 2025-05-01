193630f57cfb29049aba6aa5c6d523b369925378
wsteadman <steady.william@gmail.com>
2025-05-01 00:07:44 -0400

    indents added

--- MLUnsupervisedApp/main.py
+++ MLUnsupervisedApp/main.py
@@ -249,16 +249,16 @@ if st.sidebar.button("Run Clustering"):
         st.markdown("""
             To help visualize your dataset, we used a technique called **PCA (Principal Component Analysis)**.  
             Most datasets have many features (columns).
-                - PCA looks for the directions (combinations of features) where the data varies the most.
-                - It then **rotates and compresses** the data along those directions to create a 2D view.
+                    - PCA looks for the directions (combinations of features) where the data varies the most.
+                    - It then **rotates and compresses** the data along those directions to create a 2D view.
 
             Each dot in the plot below represents a data point, and the color shows which **cluster** it was assigned to by the KMeans algorithm.
 
             How the KMeans algorithm works:
-                - It first randomly picks **k** points as cluster centers (called centroids).
-                - Each data point is assigned to the nearest centroid based on distance.
-                - The centroids are then reassigned to the average position of the points assigned to them.
-                - This repeats until the groupings stop changing significantly.
+                    - It first randomly picks **k** points as cluster centers (called centroids).
+                    - Each data point is assigned to the nearest centroid based on distance.
+                    - The centroids are then reassigned to the average position of the points assigned to them.
+                    - This repeats until the groupings stop changing significantly.
                         """)
     with tab2:
         st.subheader("Finding the Optimal Number of Clusters")
